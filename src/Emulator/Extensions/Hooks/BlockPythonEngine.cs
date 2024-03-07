7f09a9cea709666d5cd74f5c37cae74cfe78afe7
asmitaj08 <asmita.jha82@gmail.com>
2024-03-07 13:26:35 -0800

    cleaning

--- src/Emulator/Extensions/Hooks/BlockPythonEngine.cs
+++ src/Emulator/Extensions/Hooks/BlockPythonEngine.cs
@@ -18,39 +18,6 @@ using System.Runtime.InteropServices;
 
 namespace Antmicro.Renode.Hooks
 {
-    // public class LibAflInterop
-    // {  
-        
-    //     // Import the Rust function
-    //     // [DllImport("../../../../../../../LibAFL/fuzzers/libafl_renode/target/release/liblibafl_renode.so")]
-    //     [DllImport("/home/asmita/fuzzing_bare-metal/SEFF_project_dirs/SEFF-project/LibAFL/fuzzers/libafl_renode/target/release/liblibafl_renode.so")] 
-    //     public static extern IntPtr get_cov_map_ptr(); 
-    //     // You can create a C# array that mirrors the Rust array
-        
-    //     //static ulong PREV_LOC = 0;
-    //     public const int MAP_SIZE = 8 * 1024;
-    //     public static IntPtr covMapPtr = get_cov_map_ptr(); 
-       
-    //    // public static void block_hook(ulong address)
-    //   //  {
-    //     //    //unsafe
-    //    //     {
-    //      //       ulong hash = (address ^ PREV_LOC) & (MAP_SIZE - 1);
-    //     //       // IntPtr EDGES_MAP_PTR = get_cov_map_ptr();
-    //      //       Console.WriteLine($"cov-map : {covMapPtr}");
-    //      //       // Console.WriteLine($"Block hook: 0x{address:X}\t size:{small:X} hash: {hash:X}");
-
-    //       //      // Assuming EDGES_MAP is an array declared globally
-    //       //      //covMapPtr[(int)hash] += 1;
-    //       //     //Marshal.ReadInt32(covMapPtr, (int)hash) += 1; // Assuming you want to access the element at index 2
-    //         //    //elementAtIndex += 1;
-    //        //     int newValue = Marshal.ReadInt32(covMapPtr + (int)hash * sizeof(int)) + 1;
-    //        //     Marshal.WriteInt32(covMapPtr + (int)hash * sizeof(int), newValue);
-
-    //        //     PREV_LOC = address >> 1;
-    //       //  }
-    //    // }
-    // }
     public sealed class BlockPythonEngine : PythonEngine
     {
         // static byte[] CovMap = new byte[8 * 1024];
@@ -84,14 +51,10 @@ namespace Antmicro.Renode.Hooks
                 Scope.SetVariable("pc", pc);
                 Scope.SetVariable("size", size);
                 covMapPtr = get_cov_map_ptr();
-                //update_cov_map(pc);
-                //Marshal.Copy(covMapPtr, CovMap, 0, CovMap.Length);
                 ulong hash = (pc ^ PREV_LOC) & (MAP_SIZE - 1);
                 byte newValue = Marshal.ReadByte(covMapPtr + (int)hash * sizeof(int));
                 byte prev_new_val = newValue;
                 newValue++;
-                // index=(int)hash * sizeof(int);
-                // Console.WriteLine($"Index : {index},hash : {hash} , sizeofInt : {sizeof(int)}, newvalue2 : {newValue}");
                 Marshal.WriteByte(covMapPtr + (int)hash * sizeof(int), newValue);
                 // CovMap[index] = newValue;
                 PREV_LOC = pc >> 1;
